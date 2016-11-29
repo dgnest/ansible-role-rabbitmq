a2930b6a07bd8932fd044eacb12f4db8c2bcdc54
@slovacus <slovacus@gmail.com>
2016-11-29 15:35:42 -0500

    [#7] Add: settings script

--- script/provision.sh
+++ script/provision.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+# shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
-echo "execute Provision"
-
-FILE_PLAYBOOK="$PROVISION_DIR/provision.yaml"
-PATH_INVENTORY="$PROVISION_DIR/inventories/aws/production"
-USER=$DEPLOY_ACCOUNT
-PRIVATE_KEY="$KEY_FILE"
-EXTRA_VARS="@$PROVISION_DIR/vars/production.yaml"
-
-ansible-playbook $FILE_PLAYBOOK -i $PATH_INVENTORY  -v \
-                 --user=$USER  --private-key=$PRIVATE_KEY \
-                 --extra-vars $EXTRA_VARS \
+ansible-playbook "${FILE_PLAYBOOK}" -i "${PATH_INVENTORY}"  -v \
+                 --user="${USER}"  --private-key="${PRIVATE_KEY}" \
+                 --extra-vars "${EXTRA_VARS}" \
                  --vault-password-file ~/.vault_pass.txt
